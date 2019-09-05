8987691b9f73e86f662812391b94155e74f57cd4
velopert <public.velopert@gmail.com>
2019-09-06 08:49:24 +0900

    Implement without breaking type

--- src/api/github.ts
+++ src/api/github.ts
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 export async function getUserProfile(username: string) {
   // Generic 을 통해 응답 데이터의 타입을 설정 할 수 있습니다.
-  const response = await axios.get<GithubProfile>(
-    `https://api.github.com/users/${username}`
-  );
+  const response = await axios.get<GithubProfile>(`https://api.github.com/users/${username}`);
   return response.data; // 데이터 값을 바로 반환하도록 처리합니다.
 }
 
--- src/lib/createAsyncSaga.ts
+++ src/lib/createAsyncSaga.ts
@@ -1,22 +1,28 @@
 import { call, put } from 'redux-saga/effects';
-import { AsyncActionCreator } from 'typesafe-actions';
-
-type AnyAsyncActionCreator = AsyncActionCreator<any, any, any>;
-type PromiseCreatorFunction<P, T> = (payload: P) => Promise<T>;
+import { AsyncActionCreator, PayloadAction } from 'typesafe-actions';
 
 /* 
   유틸함수의 재사용성을 높이기 위하여 함수의 파라미터는 언제나 하나의 값을 사용하도록 하고,
   action.payload 를 그대로 파라미터로 넣어주도록 설정합니다.
   만약에 여러가지 종류의 값을 파라미터로 넣어야 한다면 객체 형태로 만들어줘야 합니다.
 */
+type PromiseCreatorFunction<P, T> = ((payload: P) => Promise<T>) | (() => Promise<T>);
 
-export default function createAsyncSaga<AC extends AnyAsyncActionCreator, P, T>(
-  asyncActionCreator: AC,
-  promiseCreator: PromiseCreatorFunction<P, T>
+// action 이 payload 를 갖고 있는지 확인합니다.
+// __ is __ 문법은 Type guard 라고 부릅니다 https://www.typescriptlang.org/docs/handbook/advanced-types.html#type-guards-and-type-assertions
+function isPayloadAction<P>(action: any): action is PayloadAction<string, P> {
+  return action.payload !== undefined;
+}
+
+export default function createAsyncSaga<T1, P1, T2, P2, T3, P3>(
+  asyncActionCreator: AsyncActionCreator<[T1, P1], [T2, P2], [T3, P3]>,
+  promiseCreator: PromiseCreatorFunction<P1, P2>
 ) {
   return function* saga(action: ReturnType<typeof asyncActionCreator.request>) {
     try {
-      const result = yield call(promiseCreator, action.payload);
+      const result = isPayloadAction<P1>(action)
+        ? yield call(promiseCreator, action.payload)
+        : yield call(promiseCreator);
       yield put(asyncActionCreator.success(result));
     } catch (e) {
       yield put(asyncActionCreator.failure(e));
