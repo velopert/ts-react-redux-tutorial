3f2107548dab1ec11699c2664325a96134aef710
velopert <public.velopert@gmail.com>
2019-09-05 22:45:16 +0900

    Prepare saga

---
+++ src/modules/github/sagas.ts
@@ -0,0 +1,16 @@
+import { getUserProfileAsync, GET_USER_PROFILE } from './actions';
+import { getUserProfile, GithubProfile } from '../../api/github';
+import { call, put, takeEvery } from 'redux-saga/effects';
+
+function* getUserProfileSaga(action: ReturnType<typeof getUserProfileAsync.request>) {
+  try {
+    const userProfile: GithubProfile = yield call(getUserProfile, action.payload);
+    yield put(getUserProfileAsync.success(userProfile));
+  } catch (e) {
+    yield put(getUserProfileAsync.failure(e));
+  }
+}
+
+export default function* githubSaga() {
+  yield takeEvery(GET_USER_PROFILE, getUserProfileSaga);
+}
