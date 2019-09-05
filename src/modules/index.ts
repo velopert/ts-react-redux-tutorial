962db32a67c65aa4665ccfb027cea331e3a1a89a
velopert <public.velopert@gmail.com>
2019-09-06 01:00:20 +0900

    import default from github

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import todos from './todos';
-import github from './github/reducer';
+import github from './github';
 
 const rootReducer = combineReducers({
   counter,
